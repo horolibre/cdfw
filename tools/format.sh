a81a0cc55394c527593ef3827e3a0c08378c8eb8
Ian Dinwoodie <dinwoodieian@gmail.com>
2025-01-03 19:35:25 -0500

    Added clang format config and script.

---
+++ tools/format.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2025 Ian Dinwoodie. All rights reserved.
+# Use of this source code is governed by a GPLv3 license that can be found in
+# the LICENSE file.
+
+function main {
+  local script_dir
+  script_dir=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+  cd "${script_dir}/.."
+
+  find cdfw/ -iname '*.h' -o -iname '*.cpp' | xargs clang-format --style=file -i
+}
+
+main
+
