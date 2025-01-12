34e3f576fe2ce6ad470b20cb5bda7ce7cd7dc123
Ian Dinwoodie <dinwoodieian@gmail.com>
2025-01-11 21:45:18 -0500

    Added script for generating coverage reports.

--- support/macos/coverage_flags.py
+++ support/macos/coverage_flags.py
@@ -1,4 +1,18 @@
 Import("env")
 
+env_name = env["PIOENV"]
+
+def coverage_compile_flags(env, node):
+    if f"{env_name}/src" in node.get_abspath():
+        print(f"NEED COVERAGE FLAGS: {node.get_abspath()}")
+        return env.Object(
+         node,
+         CCFLAGS=env["CCFLAGS"] + ["-fprofile-instr-generate", "-fcoverage-mapping"]
+        )
+    else:
+        return node
+
+env.AddBuildMiddleware(coverage_compile_flags)
+
 env.Append(LINKFLAGS=["-fprofile-instr-generate",
                         "-fcoverage-mapping"])
---
+++ tools/gen-cov.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2025 Ian Dinwoodie. All rights reserved.
+# Use of this source code is governed by a GPLv3 license that can be found in
+# the LICENSE file.
+
+function main {
+  local script_dir
+  script_dir=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+  cd "${script_dir}/.."
+
+  # Reinitialize the coverage directory.
+  rm -rf ./coverage
+  mkdir coverage
+
+  # Generate coverage data.
+  export LLVM_PROFILE_FILE="./coverage/cdfw.%p.profraw"
+  local env="macos-cov"
+  pio test -e "${env}"
+  if [ $? -ne 0 ]; then
+    echo "Failed to generate coverage data."
+    exit 1
+  fi
+
+  # Generate the coverage report(s).
+  export PATH=/Library/Developer/CommandLineTools/usr/bin:$PATH
+  llvm-profdata merge -sparse coverage/*.profraw -o coverage/cdfw.profdata
+  if [ $? -ne 0 ]; then
+    echo "Failed to merge coverage data."
+    exit 1
+  fi
+
+  local inst_bin=.pio/build/"${env}"/program
+  llvm-cov show -instr-profile=coverage/cdfw.profdata "${inst_bin}" \
+    -sources cdfw/ > coverage/coverage_view.txt
+  if [ $? -ne 0 ]; then
+    echo "Failed to generate coverage view."
+    exit 1
+  fi
+
+  llvm-cov report -instr-profile=coverage/cdfw.profdata "${inst_bin}" \
+    -sources cdfw/ > coverage/coverage_report.txt
+  if [ $? -ne 0 ]; then
+    echo "Failed to generate coverage report."
+    exit 1
+  fi
+}
+
+main
+
